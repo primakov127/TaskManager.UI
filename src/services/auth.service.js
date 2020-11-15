1c349084f477594e2017b779045ba8f9d2fe790e
Max Primakov <primakov127@gmail.com>
2020-11-16 00:27:22 +0300

    Add general component like Login and Register

---
+++ src/services/auth.service.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8075/api/auth/";
+
+class AuthService {
+  login(username, password) {
+    return axios
+      .post(API_URL + "login", {
+        username,
+        password,
+      })
+      .then((response) => {
+        if (response.data.token) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+
+        return response.data;
+      });
+  }
+
+  logout() {
+    localStorage.removeItem("user");
+  }
+
+  register(username, email, firstName, lastName, password) {
+    return axios.post(API_URL + "register", {
+      username,
+      email,
+      firstName,
+      lastName,
+      password,
+    });
+  }
+
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem("user"));
+  }
+}
+
+export default new AuthService();
---
+++ src/services/user.service.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+import authHeader from "../helpers/auth-header";
+
+const API_URL = "http://localhost:8075/api/test/";
+
+class UserService {
+  getPublicContent() {
+    return axios.get(API_URL + "all");
+  }
+
+  getUserBoard() {
+    return axios.get(API_URL + "user", { headers: authHeader() });
+  }
+
+  getAdminBoard() {
+    return axios.get(API_URL + "admin", { headers: authHeader() });
+  }
+}
+
+export default new UserService();
